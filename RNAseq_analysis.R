77cfebad26cc797c97bdbba0b60ed3de5fb223bd
Abhilash Kannan <abhilifescizurich@gmail.com>
2016-09-22 16:53:24 +0200

    Create RNAseq_analysis.R

---
+++ RNAseq_analysis.R
@@ -0,0 +1,28 @@
+#Author: Abhilash
+# This is a code for Analysis of RNA-Seq Data with R/Bioconductor
+
+# Install and load all the packages we will need to analyse the data
+source("https://bioconductor.org/biocLite.R")
+
+biocLite("edgeR")
+biocLite("limma")
+biocLite("EDASeq")
+biocLite("Glimma")
+biocLite("gplots")
+biocLite("org.Mm.eg.db")
+biocLite("RColorBrewer")
+
+library(edgeR)
+library(limma)
+library(EDASeq)
+library(Glimma)
+library(gplots)
+library(org.Mm.eg.db)
+library(RColorBrewer)
+
+# Set the working directory which has all the files with read counts required for RNA seq analysis
+
+setwd("~/Desktop/Abhilash/RNAseq_analysis/RNA_seq_results/")
+fs <- dir(,".txt$")
+
+
